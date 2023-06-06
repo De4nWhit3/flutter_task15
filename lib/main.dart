8d7e73edc35fc55041a401cf69123059243ad92d
Dean White <de4nwhit3@gmail.com>
2023-06-06 02:40:43 +0200

    added question class and template pages with colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
+const Color limeGreen = Color.fromARGB(255, 43, 255, 0);
+const Color fadedBlack = Color.fromARGB(255, 27, 27, 27);
+const Color black = Colors.black;
 void main() {
   runApp(const MyApp());
 }
@@ -17,29 +21,30 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const QuestionHomePage(),
       routes: <String, WidgetBuilder>{
-        '/root': (BuildContext context) => const MyHomePage(),
+        '/root': (BuildContext context) => const QuestionHomePage(),
+        '/addQuestion': (BuildContext context) => const AddQuestionScreen(),
+        // '/viewSpecificQuestion':
       },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class QuestionHomePage extends StatefulWidget {
+  const QuestionHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<QuestionHomePage> createState() => _QuestionHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _QuestionHomePageState extends State<QuestionHomePage> {
   @override
   Widget build(BuildContext context) {
-    const Color limeGreen = Color.fromARGB(255, 43, 255, 0);
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 27, 27, 27),
+      backgroundColor: fadedBlack,
       appBar: AppBar(
-        backgroundColor: Colors.black,
+        backgroundColor: black,
         centerTitle: true,
         title: const Text(
           'Dean\'s 10/10 App',
@@ -52,11 +57,93 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return AnimationConfiguration.staggeredList(
+              position: index,
+              duration: const Duration(milliseconds: 3),
+              child: SlideAnimation(
+                verticalOffset: 50.0,
+                child: FadeInAnimation(
+                  child: Container(
+                    // color: limeGreen,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: limeGreen,
+                        width: 2,
+                      ),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                    ),
+                    height: 100,
+                    width: double.infinity,
+                    margin: const EdgeInsets.all(
+                      10,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+          backgroundColor: black,
+          foregroundColor: limeGreen,
+          icon: const Icon(Icons.bug_report),
+          onPressed: () {
+            Navigator.of(context).pushNamed('/addQuestion');
+          },
+          label: const Text(
+            'Add Question',
+            style: TextStyle(
+                // color: limeGreen,
+                ),
+          )),
     );
+  }
+}
+
+class AddQuestionScreen extends StatelessWidget {
+  const AddQuestionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> availableCategories = ["coding", "science", "math"];
+    return Scaffold(
+      backgroundColor: fadedBlack,
+      appBar: AppBar(
+        backgroundColor: black,
+        foregroundColor: limeGreen,
+        title: const Text(
+          'Adding a question',
+          style: TextStyle(
+              // color: limeGreen,
+              ),
+        ),
+      ),
+      body: Placeholder(),
+    );
+  }
+}
+
+class Question {
+  final String question;
+  final String answer;
+  final String category;
+  final String imageURL;
+  final List<String> hints;
+
+  Question(
+      {required this.question,
+      required this.answer,
+      required this.category,
+      required this.imageURL,
+      required this.hints});
+
+  (String, String, String, String, List<String>) getAllData() {
+    return (question, answer, category, imageURL, hints);
   }
 }
