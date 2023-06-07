a911253df22523ba766c4a567d351075a94b6a07
Dean White <de4nwhit3@gmail.com>
2023-06-07 02:46:54 +0200

    styled add question page

--- lib/widgets/add_question_screen.dart
+++ lib/widgets/add_question_screen.dart
@@ -34,32 +34,81 @@ class _AddQuestionScreenState extends State<AddQuestionScreen> {
               ),
         ),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            const Text(
-              'Enter a question:',
-              style: TextStyle(color: limeGreen),
-            ),
-            TextFormField(
-              style: const TextStyle(color: limeGreen),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a value';
-                }
-                return null;
-              },
-            ),
-            SingleChoiceWidget(
-              black: widget.black,
-              limeGreen: widget.limeGreen,
-            ),
-            QuizDropdown(
-              black: widget.black,
-              limeGreen: widget.limeGreen,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 20,
+          left: 30,
+          right: 30,
+          bottom: 20,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const Text(
+                'Enter a question:',
+                style: TextStyle(color: limeGreen, fontSize: 18),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                style: const TextStyle(color: limeGreen),
+                cursorColor: limeGreen,
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: black,
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: limeGreen,
+                      width: 2,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: limeGreen,
+                      width: 2,
+                    ),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a value';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                'How many hints would you like to add?',
+                style: TextStyle(color: limeGreen, fontSize: 18),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SingleChoiceWidget(
+                black: widget.black,
+                limeGreen: widget.limeGreen,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                'In which category is your question?',
+                style: TextStyle(color: limeGreen, fontSize: 18),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              QuizDropdown(
+                black: widget.black,
+                limeGreen: widget.limeGreen,
+              ),
+            ],
+          ),
         ),
       ),
     );
