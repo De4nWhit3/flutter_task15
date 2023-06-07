45e318dfdb7ca344a91c3ccb522492ae3d9e006c
Dean White <de4nwhit3@gmail.com>
2023-06-07 03:21:58 +0200

    n/a

--- lib/main.dart
+++ lib/main.dart
@@ -184,54 +184,60 @@ class _QuestionHomePageState extends State<QuestionHomePage> {
         ],
       ),
       body: Center(
-        child: ListView.builder(
-          itemCount: questions.length,
-          itemBuilder: (BuildContext context, int index) {
-            return AnimationConfiguration.staggeredList(
-              position: index,
-              duration: const Duration(milliseconds: 3),
-              child: SlideAnimation(
-                verticalOffset: 50.0,
-                child: FadeInAnimation(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pushNamed('/addQuestion');
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: black,
-                        border: Border.all(
-                          color: limeGreen,
-                          width: 2,
-                        ),
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(5),
-                        ),
-                      ),
-                      // height: 100,
-                      width: double.infinity,
-                      margin: const EdgeInsets.all(
-                        10,
-                      ),
-                      child: Column(
-                        children: [
-                          Image(
-                            height: 150,
-                            image: AssetImage(questions[index].imageURL),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: questions.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return AnimationConfiguration.staggeredList(
+                    position: index,
+                    duration: const Duration(milliseconds: 3),
+                    child: SlideAnimation(
+                      verticalOffset: 50.0,
+                      child: FadeInAnimation(
+                        child: GestureDetector(
+                          onTap: () {
+                            // Navigator.of(context).pushNamed('/addQuestion');
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: black,
+                              border: Border.all(
+                                color: limeGreen,
+                                width: 2,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(5),
+                              ),
+                            ),
+                            // height: 100,
+                            width: double.infinity,
+                            margin: const EdgeInsets.all(
+                              10,
+                            ),
+                            child: Column(
+                              children: [
+                                Image(
+                                  height: 150,
+                                  image: AssetImage(questions[index].imageURL),
+                                ),
+                                Text(
+                                  questions[index].question,
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(color: limeGreen),
+                                ),
+                              ],
+                            ),
                           ),
-                          Text(
-                            questions[index].question,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(color: limeGreen),
-                          ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ),
+                  );
+                },
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
       floatingActionButton: Container(
